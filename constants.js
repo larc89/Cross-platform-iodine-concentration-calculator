1f834c77cfff862270a735baf0c9d4d2b858b3d2
larc89 <91508348+larc89@users.noreply.github.com>
2021-09-27 17:24:20 -0700

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,72 @@
+const CONVERSION_FACTORS = {
+    "Rapid kV switching DECT (rs-DECT)": {
+        "Vendor-specific": {
+            "Venous": 0.884,
+            "Delayed": 0.913,
+        },
+        "Matched to rs-DECT exposure": {
+            "Venous": 0.884,
+            "Delayed": 0.913,
+        },
+        "40% reduced exposure": {
+            "Venous": 0.96,
+            "Delayed": 0.934,
+        },
+    },
+    "Dual-layer DECT (dl-DECT)": {
+        "Vendor-specific": {
+            "Venous": 1.155,
+            "Delayed": 1.236,
+        },
+        "Matched to rs-DECT exposure": {
+            "Venous": 1.174,
+            "Delayed": 1.366,
+        },
+        "40% reduced exposure": {
+            "Venous": 1.206,
+            "Delayed": 1.228,
+        },
+    },
+    "Dual-source DECT 80/150Sn kV (ds-DECT)": {
+        "Vendor-specific": {
+            "Venous": 1.074,
+            "Delayed": 1.027,
+        },
+        "Matched to rs-DECT exposure": {
+            "Venous": 1.073,
+            "Delayed": 1.121,
+        },
+        "40% reduced exposure": {
+            "Venous": 1.203,
+            "Delayed": 1.327,
+        },
+    },
+    "Dual-source DECT 90/150Sn kV (ds-DECT)": {
+        "Vendor-specific": {
+            "Venous": 1.024,
+            "Delayed": 1.149,
+        },
+        "Matched to rs-DECT exposure": {
+            "Venous": 1.078,
+            "Delayed": 1.139,
+        },
+        "40% reduced exposure": {
+            "Venous": 1.099,
+            "Delayed": 0.996,
+        },
+    },
+    "Dual-source DECT 100/150Sn kV (ds-DECT)": {
+        "Vendor-specific": {
+            "Venous": 1.059,
+            "Delayed": 1.146,
+        },
+        "Matched to rs-DECT exposure": {
+            "Venous": 0.989,
+            "Delayed": 1.125,
+        },
+        "40% reduced exposure": {
+            "Venous": 1.164,
+            "Delayed": 1.284,
+        },
+    },
+}
